1fb6056633421c85d48c0ff77fb9b89b110150e8
Esteban Carrillo <estexav0996@gmail.com>
2024-11-19 13:23:07 +0000

    Exercise Tracker done

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const usersSchema = new mongoose.Schema({
     type: String,
     required: [true, "Username is required"],
   },
-
+  count: { type: Number, default: 0 },
   log: [
     {
       description: { type: String },
@@ -52,11 +52,17 @@ const createAndSaveUser = (username) => {
   }
 };
 
-app.route("/api/users").post((req, res) => {
-  const { username } = req.body;
-  let newUser = createAndSaveUser(username);
-  return res.json(newUser);
-});
+app
+  .route("/api/users")
+  .get(async (req, res) => {
+    const users = await Users.find();
+    return res.json(users);
+  })
+  .post((req, res) => {
+    const { username } = req.body;
+    let newUser = createAndSaveUser(username);
+    return res.json(newUser);
+  });
 
 const findPersonById = async (id) => {
   return await Users.findById(id)
@@ -76,8 +82,11 @@ app.route("/api/users/:_id/exercises").post(async (req, res) => {
 
   let newLog = {
     description: description,
-    duration: duration,
-    date: new Date(date).toDateString(),
+    duration: Number(duration),
+    date:
+      date === "" || date === undefined
+        ? new Date().toDateString()
+        : new Date(date).toDateString(),
   };
 
   const user = await findPersonById(_id);
@@ -87,33 +96,71 @@ app.route("/api/users/:_id/exercises").post(async (req, res) => {
   let error = user.validateSync();
 
   if (error !== undefined) return res.json({ Error: error.message });
+  user.count = user.log.length;
   user.save();
-  return res.json({ _id: user._id, username: user.username, ...newLog });
+  return res.json({ username: user.username, ...newLog, _id: user._id });
 });
 
-app.get("/api/users/:id/logs", async (req, res) => {
+function convertDateType(date) {
+  if (date?.match(/^(\d+)$/)) {
+    return new Date(Number(date));
+  } else {
+    return new Date(date);
+  }
+}
+
+app.route("/api/users/:id/logs").get(async (req, res) => {
   const { id } = req.params;
   const { from, to, limit } = req.query;
 
-  console.log(limit);
+  console.log(req.query);
 
-  const user = await Users.find({ _id: id })
-    .sort({ username: 1 })
-    .select({ __v: 0 })
-    .limit(Number(limit))
-    .exec();
-  console.log(user);
+  const fromDate = convertDateType(from);
+  const toDate = convertDateType(to);
 
-  // console.log(req.query)
+  console.log({ from: fromDate, to: toDate });
 
-  // const user = await findPersonById(id)
-  // if (user.error) return res.json(user.error);
-  // let userLog = [...user.log]
-  // userLog = userLog.map((e)=>{
-  //   return{description:e.description, duration:e.duration, date:new Date(e.date).toDateString()}
-  // });
+  const user = await findPersonById(id);
+  if (user.error) return res.json(user.error);
+  let userLog = [...user.log];
 
-  // res.json({_id:user._id,username:user.username, count:user.__v,log:userLog})
+  let fromAndToDate = {};
+
+  if (
+    fromDate.toDateString() !== "Invalid Date" &&
+    toDate.toDateString() !== "Invalid Date"
+  ) {
+    fromAndToDate = {
+      from: fromDate.toDateString(),
+      to: toDate.toDateString(),
+    };
+    userLog = userLog.filter((n) => n.date > fromDate && n.date < toDate);
+  } else if (fromDate.toDateString() !== "Invalid Date") {
+    fromAndToDate = { from: fromDate.toDateString() };
+    userLog = userLog.filter((n) => n.date > fromDate);
+  } else if (toDate.toDateString() !== "Invalid Date") {
+    fromAndToDate = { to: toDate.toDateString() };
+    userLog = userLog.filter((n) => n.date < toDate);
+  }
+
+  userLog = userLog.map((e) => {
+    return {
+      description: e.description,
+      duration: e.duration,
+      date: new Date(e.date).toDateString(),
+    };
+  });
+
+  limit ? userLog.splice(limit) : 0;
+
+  const { __v, log: _, ...userInfo } = user._doc;
+
+  let resObject = {
+    ...userInfo,
+    ...fromAndToDate,
+    log: userLog,
+  };
+  res.json(resObject);
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
